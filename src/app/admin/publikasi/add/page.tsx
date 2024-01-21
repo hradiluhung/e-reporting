edec4c467b29d11bca97ab45b7167c51f2fbecee
Hakam Royhan Adiluhung <hakamra107@gmail.com>
2024-01-21 14:26:10 +0700

    add feature for publikasi

--- src/app/admin/publikasi/add/page.tsx
+++ src/app/admin/publikasi/add/page.tsx
@@ -1,11 +1,16 @@
 "use client"
-import { WidgetTypes } from "@/constants/button-types"
+import FilledButton from "@/components/buttons/FilledButton"
+import InputField from "@/components/input/InputField"
+import { WidgetSizes, WidgetTypes } from "@/constants/button-types"
 import { createPublikasi } from "@/controllers/publikasi-controller"
 import { compressFile } from "@/helpers/imageComporession"
 import { showToast } from "@/helpers/showToast"
 import { uploadPhoto } from "@/helpers/uploadPhotos"
+import Image from "next/image"
+import Link from "next/link"
 import { useRouter } from "next/navigation"
 import React, { FormEvent, useState } from "react"
+import { ArrowLeftCircle, Trash2 } from "react-feather"
 
 export default function Page() {
   const router = useRouter()
@@ -89,5 +94,64 @@ export default function Page() {
     }
   }
 
-  return <div>Tambah</div>
+  return (
+    <div className="w-full px-4 py-4 md:px-8 lg:px-20 lg:py-4">
+      <div className="flex flex-col items-start gap-8">
+        <div className="w-full flex flex-col items-start justify-center gap-6 md:w-1/3 lg:w-1/2">
+          <div className="flex gap-3 items-center">
+            <div>
+              <Link href="/admin/profil" passHref>
+                <ArrowLeftCircle className="cursor-pointer w-6 stroke-primary-100" />
+              </Link>
+            </div>
+            <h1 className="font-bold text-2xl text-transparent bg-clip-text bg-gradient-to-r from-primary-100 to-secondary-50">
+              Tambah Publikasi
+            </h1>
+          </div>
+
+          <form
+            method="POST"
+            onSubmit={onSubmit}
+            className="flex flex-col gap-3 w-full"
+          >
+            {!image ? (
+              <InputField
+                label="Gambar"
+                acceptedFileTypes="image/*"
+                placeholder="Pilih Gambar"
+                type="file"
+                onChange={onChangeInputFile}
+                size={WidgetSizes.MEDIUM}
+                value={inputPublikasi.image}
+                isRequired={false}
+              />
+            ) : (
+              <div className="w-full">
+                <label className="text-neutral-500 text-sm">Gambar</label>
+                <div className="relative w-full rounded-md border border-neutral-50 bg-neutral-0">
+                  <Image
+                    width={0}
+                    height={0}
+                    sizes="100vw"
+                    src={URL.createObjectURL(image)}
+                    alt="Cover Mata Kuliah"
+                    className="w-full object-cover rounded-md"
+                  />
+
+                  <div className="absolute top-2 right-2">
+                    <FilledButton
+                      ButtonIcon={Trash2}
+                      onClick={() => setImage(null)}
+                      size={WidgetSizes.MEDIUM}
+                      type={WidgetTypes.ERROR}
+                    />
+                  </div>
+                </div>
+              </div>
+            )}
+          </form>
+        </div>
+      </div>
+    </div>
+  )
 }
